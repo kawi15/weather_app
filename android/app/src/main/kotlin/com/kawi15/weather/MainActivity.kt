acc2969cdd5226713d0b5a139e9ecda09b7e7b9f
kawi15 <karol.m.wisniewski15@gmail.com>
2025-10-23 17:09:22 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/kawi15/weather/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kawi15.weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/presentation/pages/weather_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+
+        ),
+      ),
+    );
+  }
+}
