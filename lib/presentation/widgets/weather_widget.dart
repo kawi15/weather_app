a3df970f531591fbb0fe1f0bb55d01839f03b95f
kawi15 <karol.m.wisniewski15@gmail.com>
2025-10-24 21:47:15 +0200

    UI changes

--- lib/presentation/widgets/weather_widget.dart
+++ lib/presentation/widgets/weather_widget.dart
@@ -25,7 +25,7 @@ class WeatherWidget extends StatelessWidget {
           ),
           boxShadow: [
             BoxShadow(
-              color: Colors.deepPurple.withOpacity(0.3),
+              color: Colors.deepPurple.withValues(alpha: 0.3),
               blurRadius: 10,
               offset: const Offset(0, 5),
             ),
